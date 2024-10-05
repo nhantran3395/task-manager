b6be9ea2ac95fcdea73acaa552c9e485a07dbe40
nhantran3395 <thanhnhan33.95@gmail.com>
2024-10-05 21:11:09 +0700

    chore: add security requirement to open api doc

--- services/task_management/src/main/java/com/nhantran/task_management/config/OpenApiDocConfiguration.java
+++ services/task_management/src/main/java/com/nhantran/task_management/config/OpenApiDocConfiguration.java
@@ -1,10 +1,18 @@
 package com.nhantran.task_management.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Task Management Service API"))
+@SecurityScheme(type = SecuritySchemeType.OAUTH2, name = "bearer_token", in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(
+        info = @Info(title = "Task Management Service API"),
+        security = { @SecurityRequirement(name = "bearer_token") }
+)
 public class OpenApiDocConfiguration {
 }
