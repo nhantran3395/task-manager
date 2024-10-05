1368d607a203894c3e7c95292c13cb475f53525a
nhantran3395 <thanhnhan33.95@gmail.com>
2024-10-05 21:37:44 +0700

    chore: add caching for user query

--- services/task_management/src/main/java/com/nhantran/task_management/persistence/adapter/UserInfoPersistencePortAdapter.java
+++ services/task_management/src/main/java/com/nhantran/task_management/persistence/adapter/UserInfoPersistencePortAdapter.java
@@ -5,6 +5,7 @@ import com.nhantran.task_management.persistence.mapper.UserMapper;
 import com.nhantran.task_management.port.out.UserInfoPersistencePort;
 import com.nhantran.task_management.persistence.repository.UserJpaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -15,6 +16,7 @@ public class UserInfoPersistencePortAdapter implements UserInfoPersistencePort {
     private final UserJpaRepository userRepository;
 
     @Override
+    @Cacheable("users")
     public Optional<User> findUser(String externalId) {
         return userRepository.findByExternalId(externalId)
                 .map(UserMapper::toUser);
